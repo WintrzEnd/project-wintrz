4097803381ec6a795d3d3daddff6743592e63ad3
Johan Lindberg <valley12records@gmail.com>
2025-06-03 00:50:31 +0200

    Added item class and minor adjustments in room and player classes.

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ class Room:
 	def __init__(self, name, description):
 		self.name = name
 		self.description = description
+		self.exits = {}
 
-		# TODO: Add exits between rooms
+	# TODO: Add exits between rooms
 
 room1 = Room("Main Hub", "You are standing in the main hub. Where different portals lead to different worlds.")
 room2 = Room("Cybercafé", "You are standing in the cybercafé of the main hub. The smell of freshly brewed coffee fills the air.")
@@ -14,7 +15,17 @@ room3 = Room("CyDen", "You are in the CyDen, a meeting point for hackers and pro
 class Player:
 	def __init__(self, name):
 		self.name = name
+		self.currentRoom = []
+
+	def move():
+		pass
+
 
 	# TODO: Add movement logic between rooms as well as a locator for what room the player is in
 
 player = Player("Player")
+
+
+class Item:
+	def __init__(self, name):
+		self.name = name
